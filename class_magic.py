85678d82ace3ae094da95eebe10c2133d7e422d6
Mirazum Parvin <43598791+Mirazum@users.noreply.github.com>
2019-02-04 15:23:31 +0200

    first push

---
+++ class_magic.py
@@ -0,0 +1,8 @@
+class Magic:
+    """
+    this Magic has name, mp cost, damage,type
+    """
+    def__init(self,name, mp_cost, damage, magic_type):
+        self.name = name
+        self.mp_cost= mp_cost
+        self.damage= damage
